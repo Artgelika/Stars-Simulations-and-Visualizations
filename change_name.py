20b55d9f79105f893b7944bc5bf295b57db1c526
Angelika Przeliorz <Artgelika@gmail.com>
2021-02-17 16:02:37 +0100

    Unification of names

--- change_name.py
+++ change_name.py
@@ -1,18 +1,20 @@
 # necessary library:
 import os
 
-# zmiana nazwy na NO:______________________________________________________
-pathh = r"C:/Users/angel/AppData/Local/Programs/Python/Sieci VSCode/sieci/dwanascie_projektow/Stars/input" 
+
+pathh = r".../Stars/input" 
 filenames = os.listdir(pathh)
 print(filenames)
+
 
 # create a list from 0 to amount of filenames - 1
 # I want to create an iterator which iter step by step over files 
 order = [x for x in range(len(filenames))]
 iterator = iter(order)
 
+
 for filename in filenames:
-    try:   # ponizej kluczowa linijka
+    try:   
         os.rename(os.path.join(pathh, filename), # <- old name, below new name
                   os.path.join(pathh, "C" + str(next(iterator)) + ".csv"))
     except:
